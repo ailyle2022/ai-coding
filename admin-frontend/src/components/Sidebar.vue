3a03b1a0a588520c4c16b6cb110a921782a21672
Aily Le <aily.le@on-running.com>
2025-12-10 15:00:17 +0800

    Update Sidebar.vue

--- admin-frontend/src/components/Sidebar.vue
+++ admin-frontend/src/components/Sidebar.vue
@@ -13,7 +13,7 @@
         <li>
           <a 
             href="#" 
-            class="nav-link flex items-center p-3 rounded-xl transition duration-150 ease-in-out"
+            class="nav-link flex items-center p-3 rounded-xl transition duration-150 ease-in-out text-sm"
             :class="{'bg-secondary-bg text-primary-accent font-semibold': isActive('/'), 'text-gray-500 hover:bg-gray-100': !isActive('/')}"
             @click.prevent="navigateTo('/')"
           >
@@ -27,7 +27,7 @@
         <li>
           <a 
             href="#" 
-            class="nav-link flex items-center p-3 rounded-xl transition duration-150 ease-in-out mt-1"
+            class="nav-link flex items-center p-3 rounded-xl transition duration-150 ease-in-out mt-1 text-sm"
             :class="{'bg-secondary-bg text-primary-accent font-semibold': isActive('/users'), 'text-gray-500 hover:bg-gray-100': !isActive('/users')}"
             @click.prevent="navigateTo('/users')"
           >
@@ -41,7 +41,7 @@
         <li>
           <a 
             href="#" 
-            class="nav-link flex items-center p-3 rounded-xl transition duration-150 ease-in-out mt-1"
+            class="nav-link flex items-center p-3 rounded-xl transition duration-150 ease-in-out mt-1 text-sm"
             :class="{'bg-secondary-bg text-primary-accent font-semibold': isActive('/roles'), 'text-gray-500 hover:bg-gray-100': !isActive('/roles')}"
             @click.prevent="navigateTo('/roles')"
           >
@@ -53,7 +53,7 @@
         </li>
         <!-- 日志查看 -->
         <li>
-          <a href="#" class="flex items-center p-3 rounded-xl text-gray-500 hover:bg-gray-100 transition duration-150 ease-in-out mt-1">
+          <a href="#" class="flex items-center p-3 rounded-xl text-gray-500 hover:bg-gray-100 transition duration-150 ease-in-out mt-1 text-sm">
             <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path>
             </svg>
